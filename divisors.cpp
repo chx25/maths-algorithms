818d9703bc3e10c550b12f0eb95913ec7dc15665
Christ Chamoun <christchamoun993@gmail.com>
2025-11-20 10:52:16 +0200

    initialise quotient to the value of num inside for loop

--- divisors.cpp
+++ divisors.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int num,quotient;
+    int num;
     cout << "Enter number: ";
     cin >> num;
     cout << "Divisors of " << num << " ={";
-    for(int i{1};i<quotient;i++){
+    for(int i{1},quotient{num};i<quotient;i++){
         if(num%i != 0) continue;
         else{
             quotient = num/i;
