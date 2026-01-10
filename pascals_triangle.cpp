5f5c8d93000a4bb6b31a70d16a6160a3585fc7eb
Christ Chamoun <christchamoun993@gmail.com>
2026-01-11 00:21:04 +0200

    finalyse pascalsgenerator function and fix loop condition to prevent off-by-one error

--- pascals_triangle.cpp
+++ pascals_triangle.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 vector<int> pascalsGenerator(int power){
     vector<int> tempRow1={1,1},tempRow2={1,1};//both initialized to the 1st row
-    for(int i{1};i<=power;i++){//iterate over rows till needed power is reached
+    for(int i{1};i<power;i++){//iterate over rows till needed power is reached
         tempRow2.clear();//go to the next row
         tempRow2.push_back(1);//set the first number to 1
         for(int j{0};j<tempRow1.size()-1;j++){//add each number of the row to the one after it
             tempRow2.push_back(tempRow1[j]+tempRow1[j+1]);
         }
+        tempRow2.push_back(1);//add the last number as 1
+        tempRow1 = tempRow2;
     }
     return tempRow2;
 }
