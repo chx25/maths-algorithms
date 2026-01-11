b8f317f8686c84f45a7da523af685e97d4291dd8
Christ Chamoun <christchamoun993@gmail.com>
2026-01-11 14:12:41 +0200

    Add optional parameter to pascalsGenerator for row output

--- pascals_triangle.cpp
+++ pascals_triangle.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-vector<int> pascalsGenerator(int power){
+vector<int> pascalsGenerator(int power,bool pastRows=false){
     vector<int> tempRow1={1,1},tempRow2={1,1};//both initialized to the 1st row
     for(int i{1};i<power;i++){//iterate over rows till needed power is reached
         tempRow2.clear();//go to the next row
@@ -12,10 +12,17 @@ vector<int> pascalsGenerator(int power){
         }
         tempRow2.push_back(1);//add the last number as 1
         tempRow1 = tempRow2;
+        if(pastRows){
+            for(int num : tempRow2) cout<<num<<" ";
+            cout<<endl;
+        } 
     }
     return tempRow2;
 }
 int main(){
-    
+    int maxPower;
+    cout << "what power do you wanna reach?: ";
+    cin >>maxPower;
+    pascalsGenerator(maxPower,true);
     return 0;
 }
